e57856846dc196d29954b5a4e16c1a86ab828fc0
Yulia <yulia1201556@gmail.com>
2022-12-03 01:33:29 +0300

    Задание 1 Возведение числа в степень

--- task001.java
+++ task001.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 
 public class task001 {
  public static void main(String[] args) throws IOException {
-    calculate_and_write();
+    count();
  }
- static String[] read()throws IOException{
+ static String[] read() throws IOException {
     ArrayList<String> list = new ArrayList<>();
     try(Scanner sc = new Scanner(new File("input.txt"))){
         while(sc.hasNextLine()){
@@ -19,39 +19,42 @@ public class task001 {
     }
     
  }
- static void calculate_and_write()throws IOException{
+ static void count()throws IOException{
     String[] newArr = read();
-    String [] a1 = newArr[0].split(" ");
-    String [] b1 = newArr[1].split(" ");
+    String[] a1 = newArr[0].split(" ");
+    String[] b1 = newArr[1].split(" ");
     double a = Double.parseDouble(a1[1]);
     double b = Double.parseDouble(b1[1]);
     double sum = power_modul(a, b);
-    if(sum==0){
+    if(sum == 0) {
         System.out.println("Не определено");
-        FileWriter fw = new FileWriter("output.txt",false);
+        FileWriter fw = new FileWriter("output.txt", false);
         fw.write("Не определено");
         fw.flush();
-    }else{
+    }
+    else {
     System.out.println(a+"^"+" "+b+" = "+sum);
-    FileWriter fw = new FileWriter("output.txt",false);
-    fw.write("Result:"+sum);
+    FileWriter fw = new FileWriter("output.txt", false);
+    fw.write("Result:" +sum);
     fw.flush();
-    System.out.println("Succesfully wrote to the file");
+    System.out.println("Записано в файл output.txt");
     }
  }
- static double power_modul(double a,double b){
-    double power=1;
-    if(b>0){
-        for (int i = 1; i <=b; i++) {
-            power*=a;
+ static double power_modul(double a, double b) {
+    double power = 1;
+    if(b > 0) {
+        for (int i = 1; i <= b; i++) {
+            power *= a;
         }
     return power;
-    }else if(a==0 && b==0){
+    } 
+    else if(a == 0 && b == 0){
         System.out.println("Не определено");
         return 0;
-    }else{
+    }
+    else {
         for (int i = 0; i > b; i--) {
-            power*=a;
+            power *= a;
         }
         power = 1/power;
     }
